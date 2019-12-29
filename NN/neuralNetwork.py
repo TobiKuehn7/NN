09a50a1fed1522046b76463ea01f894c88475b20
Tobias Kühn <tobias.kuehn@ges.thm.de>
2019-12-29 18:37:37 +0100

    finished NN

--- NN/neuralNetwork.py
+++ NN/neuralNetwork.py
@@ -23,12 +23,29 @@ class neuralNetwork:
         pass
 
     # train NN
-    def train(self):
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        numpy.transpose(hidden_outputs))
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        numpy.transpose(inputs))
+
         pass
 
     # query NN
     def query(self, inputs_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
+
         hidden_inputs = numpy.dot(self.wih, inputs)
         hidden_outputs = self.activation_function(hidden_inputs)
 

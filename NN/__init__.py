12d25b0765f3ae133055bbb75d47431c7816220f
Tobias Kühn <tobias.kuehn@ges.thm.de>
2019-12-29 18:21:09 +0100

    finished init and started query function of NN

--- NN/__init__.py
+++ NN/__init__.py
@@ -6,7 +6,8 @@ hidden_nodes = 3
 output_nodes = 3
 
 # learning rate of NN
-learning_rate = 0.5
+learning_rate = 0.3
 
 # create instance
 n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+print(n.query([1.0, 0.5, -1.5]))
--- NN/neuralNetwork.py
+++ NN/neuralNetwork.py
@@ -1,4 +1,8 @@
+import numpy
+import scipy.special
 # initialise the neural network class
+
+
 class neuralNetwork:
 
     # initialise the neural network (NN)
@@ -8,14 +12,27 @@ class neuralNetwork:
         self.hnodes = hiddennodes
         self.onodes = outputnodes
 
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
         # set learning rate
         self.lr = learningrate
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+
         pass
 
     # train NN
-    def train():
+    def train(self):
         pass
 
     # query NN
-    def query():
-        pass
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
